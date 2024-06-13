975a412efcc5923ceb5cc5bc14aa9cac1e95e57d
wanghaisheng <edwin.uestc@gmail.com>
2024-06-13 12:20:22 +0800

    fx

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Backend API",
@@ -7,6 +8,18 @@ app = FastAPI(
     docs_url="/api/",
 )
 
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.get("/api/greet")
 
 
