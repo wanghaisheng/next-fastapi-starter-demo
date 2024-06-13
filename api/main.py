78d32225f8b6aa4a0b525539afbe71603435cdbc
wanghaisheng <edwin.uestc@gmail.com>
2024-06-14 03:03:38 +0800

    fix401

--- api/main.py
+++ api/main.py
@@ -8,18 +8,6 @@ app = FastAPI(
     docs_url="/api/",
 )
 
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 app.get("/api/greet")
 
 
