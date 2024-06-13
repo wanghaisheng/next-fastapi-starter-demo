1752999a1c4e9446f516bc81e9d676708771ef0f
wanghaisheng <edwin.uestc@gmail.com>
2024-06-14 03:26:11 +0800

    fix401

--- api/main.py
+++ api/main.py
@@ -22,3 +22,13 @@ app.get("/api/ahref")
 def ahref():
     """Returns a greeting"""
     return {"message": "Hello from the backend!"}
+
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
